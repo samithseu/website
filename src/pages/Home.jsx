db904ee4ac06515a0641e271406f8b342b08cb70
Samith Seu <59063392+samithseu@users.noreply.github.com>
2024-10-11 14:32:36 +0700

    updating social icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
+import GitHub from "../components/GitHub";
+import Linkedin from "../components/LinkedIn";
+import Twitter from "../components/Twitter";
+import Telegram from "../components/Telegram";
+import Email from "../components/Email";
+
 function Home() {
   return (
     <>
@@ -18,34 +24,34 @@ function Home() {
             href="https://github.com/samithseu"
             aria-label="Click this link to checkout Samith's Github"
           >
-            <i className="fa-brands fa-github"></i>
+            <GitHub />
           </a>
           <a
             target="_blank"
             href="https://twitter.com/seumith"
             aria-label="Click this link to checkout Samith's Twitter/X"
           >
-            <i className="fa-brands fa-x-twitter"></i>
+            <Twitter />
           </a>
           <a
             target="_blank"
             href="https://www.linkedin.com/in/samith-seu-500193205/"
             aria-label="Click this link to checkout Samith's Linkedin profile"
           >
-            <i className="fa-brands fa-linkedin"></i>
+            <Linkedin />
           </a>
           <a
             target="_blank"
             href="https://t.me/samithseu"
             aria-label="Click this link to checkout Samith's Telegram"
           >
-            <i className="fa-brands fa-telegram"></i>
+            <Telegram />
           </a>
           <a
             href="mailto:seusamith@gmail.com"
             aria-label="Click this link to checkout Samith's Email"
           >
-            <i className="fa-solid fa-envelope"></i>
+            <Email />
           </a>
         </div>
       </div>
